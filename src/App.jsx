9ac2c1f6a5fa4f0747c7f9ff8cba49bede7b14be
Sergey <sergsmile29@gmail.com>
2024-12-03 21:39:43 +0200

    collections, map

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Message from './components/Message/Message';
+import User from './components/User/User';
 import WelcomeSection from './components/WelcomeSection/WelcomeSection';
-
+import usersData from './assets/users.json';
 const App = () => {
-  const isOnline = false;
-  const isLoggedIn = true;
-  const message = 'Hello react';
+  // const isOnline = false;
+  // const isLoggedIn = true;
+  // const message = 'Hello react';
   return (
     <>
       <Header />
-      <h3>Tag</h3>
+      {/* <h3>Tag</h3>
       {isOnline && <h2>Hello</h2>}
       {isLoggedIn && <h2>Welcome</h2>}
-      <p>adsfadsf {message} sdasfssfasdf</p>
-      <WelcomeSection />
-      <Footer />
+      <p>adsfadsf {message} sdasfssfasdf</p> */}
+      {/* <Message message='Привіт!' />
+      <Message author='Olena' message='Як справи?' isOnline={false} />
+      <Message author='Petro' message='Файно!' /> */}
+      <ul>
+        {/* {[1, 2, 3, 4].map(value => (
+          <li>{value}</li>
+        ))} */}
+        <User />
+        {usersData.map(user => (
+          <User key={user.id} {...user} />
+        ))}
+      </ul>
     </>
   );
 };
