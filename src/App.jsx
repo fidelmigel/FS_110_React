a276bd3b6c5756f9fb3a7f6fc4d9d62bc1e5549b
Sergey <sergsmile29@gmail.com>
2024-12-03 20:58:51 +0200

    conditional render

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import WelcomeSection from './components/WelcomeSection/WelcomeSection';
+
 const App = () => {
+  const isOnline = false;
+  const isLoggedIn = true;
+  const message = 'Hello react';
   return (
     <>
-      <h2>Hello react! Blah blah blah</h2>
-      <button>Click</button>
+      <Header />
+      <h3>Tag</h3>
+      {isOnline && <h2>Hello</h2>}
+      {isLoggedIn && <h2>Welcome</h2>}
+      <p>adsfadsf {message} sdasfssfasdf</p>
+      <WelcomeSection />
+      <Footer />
     </>
   );
 };
