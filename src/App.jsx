5a7b7c367dc336020ad29df37c0c59b8394a9210
Sergey <sergsmile29@gmail.com>
2024-12-05 20:38:55 +0200

    module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import Header from './components/Header/Header';
 import User from './components/User/User';
-
+import styles from './App.module.css';
 import usersData from './assets/users.json';
 const App = () => {
   return (
     <>
       <Header />
-      <h2>Lesson 2. Styles</h2>
-      <ul>
-        <User />
+      <h2 className='title'>Lesson 2. Styles</h2>
+      <ul className='wrapper'>
         {usersData.map(user => (
           <User key={user.id} {...user} />
         ))}
       </ul>
+      <div className={styles.wrapper}>
+        <div className={styles.box}> box1</div>
+        <div className={styles.box2}> box2</div>
+      </div>
     </>
   );
 };
--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,9 +1,18 @@
-const User = ({ name, email, bio = 'This user hide own bio' }) => {
+import clsx from 'clsx';
+import styles from './User.module.css';
+const User = ({ name, email, bio = 'This user hide own bio', isOpenToWork }) => {
+  console.log(styles);
+
   return (
-    <div>
-      <h3>{name}</h3>
+    <div className={styles.card}>
+      <h3 className={styles.title}>{name}</h3>
       <p>{email}</p>
       <p>{bio}</p>
+      {/* <p className={`${styles.status} ${isOpenToWork ? styles.green : styles.red}`}>{isOpenToWork ? 'хоче працювати' : 'не шукає роботу'}</p> */}
+      <p className={clsx(styles.status, 'hello', 'world', ['style1', null, undefined, 0, false, 'style2'], isOpenToWork ? styles.green : styles.red)}>
+        {isOpenToWork ? 'хоче працювати' : 'не шукає роботу'}
+      </p>
+
       <button>Delete</button>
     </div>
   );
