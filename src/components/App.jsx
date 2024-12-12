e32534bf5dc4ec66cdd2f338729f13085a294ac5
Sergey <sergsmile29@gmail.com>
2024-12-12 20:31:33 +0200

    modal, cleanup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import { ColorPicker } from './ColorPicker/ColorPicker';
 import { Counter } from './Counter/Counter';
+import Modal from './Modal/Modal';
 import { TodoList } from './TodoList/TodoList';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
   return (
     <div>
-      {/* <h1>State</h1> */}
-      <Counter />
-      {/* <ColorPicker /> */}
       {/* <TodoList /> */}
+      <button onClick={openModal}>Open modal</button>
+      {isOpen && (
+        <Modal onClose={closeModal}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, porro quod ipsum laboriosam architecto quam in, labore deserunt natus sapiente
+          ducimus modi aut magni dicta. Necessitatibus placeat velit laudantium maiores.
+        </Modal>
+      )}
     </div>
   );
 };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,13 +1,28 @@
+import { useEffect } from 'react';
 import s from './Modal.module.css';
-const Modal = ({ children, title = 'Default modal' }) => {
+const Modal = ({ children, title = 'Default modal', onClose }) => {
+  useEffect(() => {
+    console.log('Modal is open!');
+
+    // cleanup fn
+    return () => {
+      console.log('Пока пока 😉');
+    };
+  }, []);
+
+  const handleBackdropClick = e => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
   return (
-    <div className={s.wrapper}>
+    <div className={s.wrapper} onClick={handleBackdropClick}>
       <div className={s.content}>
-        <>
-          <h1>{title}</h1>
-          <hr />
-        </>
-        <button className={s.closeBtn}>×</button>
+        <h1>{title}</h1>
+        <hr />
+        <button onClick={onClose} className={s.closeBtn}>
+          ×
+        </button>
         {children}
       </div>
     </div>
