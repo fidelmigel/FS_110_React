08cf12b760c3a2c6f3557a3dbe45b334cf25d157
Sergey <sergsmile29@gmail.com>
2024-12-17 21:06:46 +0200

    controlled form advanced

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,15 @@ import ControlledForm from './Forms/ControlledForm';
 import Form from './Forms/Form';
 
 const App = () => {
+  const onRegister = data => {
+    console.log('Data received!');
+    console.log(data);
+    console.log('User successfully registered!🚀');
+  };
   return (
     <div>
       <h1>Forms</h1>
-      <ControlledForm />
+      <ControlledForm onRegister={onRegister} />
     </div>
   );
 };
--- src/components/Forms/ControlledForm.jsx
+++ src/components/Forms/ControlledForm.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import s from './Forms.module.css';
 
-const INITIAL_STATE = { username: '', email: '', password: '', age: '' };
+const INITIAL_STATE = { username: '', email: '', course: '', level: 'junior', details: '', isTermsAccepted: false };
 
-const ControlledForm = () => {
+const ControlledForm = ({ onRegister }) => {
   const [formData, setFormData] = useState(INITIAL_STATE);
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(formData);
+
+    onRegister(formData);
     setFormData(INITIAL_STATE); //🔥
   };
 
@@ -38,7 +39,12 @@ const ControlledForm = () => {
   // };
 
   const handleChangeInput = e => {
-    const { name, value } = e.target;
+    const { name, value, type } = e.target;
+
+    if (type === 'checkbox') {
+      return setFormData(prev => ({ ...prev, [name]: !prev[name] }));
+    }
+
     setFormData(prev => ({
       ...prev,
       [name]: value,
@@ -52,15 +58,44 @@ const ControlledForm = () => {
           <span>Name:</span>
           <input className={s.input} type='text' name='username' value={formData.username} onChange={handleChangeInput} />
         </label>
+
         <label className={s.label}>
           <span>Email:</span>
           <input className={s.input} type='text' name='email' value={formData.email} onChange={handleChangeInput} />
         </label>
+
         <label className={s.label}>
-          <span>Password:</span>
-          <input className={s.input} type='password' name='password' value={formData.password} onChange={handleChangeInput} />
+          <span>Course:</span>
+          <select className={s.input} type='text' name='course' value={formData.course} onChange={handleChangeInput}>
+            <option value='backend'>Backend</option>
+            <option value='frontend'>Frontend</option>
+            <option value='fullstack'>Fullstack</option>
+            <option value='devOps'>DevOps</option>
+          </select>
         </label>
-        <button className={s.button} type='submit'>
+        <div>
+          <label>
+            <input type='radio' value='junior' checked={formData.level === 'junior'} name='level' onChange={handleChangeInput} />
+            <span>Junior</span>
+          </label>
+          <label>
+            <input type='radio' value='middle' name='level' onChange={handleChangeInput} checked={formData.level === 'middle'} />
+            <span>Middle</span>
+          </label>
+          <label>
+            <input type='radio' value='senior' name='level' onChange={handleChangeInput} checked={formData.level === 'senior'} />
+            <span>Senior</span>
+          </label>
+        </div>
+        <label className={s.label}>
+          <span>Details:</span>
+          <textarea className={s.input} name='details' value={formData.details} onChange={handleChangeInput} />
+        </label>
+        <label>
+          <input type='checkbox' name='isTermsAccepted' checked={formData.isTermsAccepted} onChange={handleChangeInput} />
+          <span>All terms and rules accepted!</span>
+        </label>
+        <button disabled={!formData.isTermsAccepted} className={s.button} type='submit'>
           Submit
         </button>
       </form>
