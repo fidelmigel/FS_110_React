aa6ff863d4159be65feb783fd1e10cc93e0d11b5
Sergey <sergsmile29@gmail.com>
2024-12-17 19:09:40 +0200

    init m3l1_Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,7 @@
-import { useState } from 'react';
-import { ColorPicker } from './ColorPicker/ColorPicker';
-import { Counter } from './Counter/Counter';
-import Modal from './Modal/Modal';
-import { TodoList } from './TodoList/TodoList';
-import Vote from './Vote/Vote';
-
 const App = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
   return (
     <div>
-      <Vote />
-      {/* <TodoList /> */}
-      {/* <input /> */}
-      {/* <button onClick={openModal}>Open modal</button> */}
-      {isOpen && (
-        <Modal title='Modal' onClose={closeModal}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, porro quod ipsum laboriosam architecto quam in, labore deserunt natus sapiente
-          ducimus modi aut magni dicta. Necessitatibus placeat velit laudantium maiores.
-        </Modal>
-      )}
+      <h1>Forms</h1>
     </div>
   );
 };
