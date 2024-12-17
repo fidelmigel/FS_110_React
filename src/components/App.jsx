24287eab493cd6b9aacbcdf8ab3e0a6d05673955
Sergey <sergsmile29@gmail.com>
2024-12-17 19:16:28 +0200

    add styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
+import Form from './Forms/Form';
+
 const App = () => {
   return (
     <div>
       <h1>Forms</h1>
+      <Form />
     </div>
   );
 };
---
+++ src/components/Forms/Form.jsx
@@ -0,0 +1,5 @@
+import s from './Forms.module.css';
+const Form = () => {
+  return <div className={s.formWrapper}></div>;
+};
+export default Form;
