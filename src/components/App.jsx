069178c8b79f9da16faeb089d7ccd2f24bcbedaa
Sergey <sergsmile29@gmail.com>
2024-12-19 18:46:21 +0200

    init m3l2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import Example from './ExampleUseId/Example';
 import ControlledForm from './Forms/ControlledForm';
-import Form from './Forms/Form';
 
 const App = () => {
   const onRegister = data => {
@@ -12,8 +10,6 @@ const App = () => {
     <div>
       <h1>Forms</h1>
       <ControlledForm onRegister={onRegister} />
-      <Example />
-      <Example />
     </div>
   );
 };
