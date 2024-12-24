dc6f7415a62fc35c180620ff92338da11acf88e4
Drynkin Sergey <sergsmile29@gmail.com>
2024-12-24 20:04:55 +0200

    add services, useEffect used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { fetchArticles } from '../services/api';
+
 const App = () => {
-  return <div></div>;
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    // axios.get('https://hn.algolia.com/api/v1/search').then(res => setArticles(res.data.hits));
+
+    const getArticlesData = async () => {
+      try {
+        const { hits } = await fetchArticles('react');
+        setArticles(hits);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getArticlesData();
+  }, []);
+
+  return (
+    <div>
+      <h2>Articles</h2>
+      <ul>
+        {articles.map(item => (
+          <li key={item.objectID}>
+            <a href={item.url} target='_blank'>
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const fetchArticles = async query => {
+  const { data } = await axios.get(`https://hn.algolia.com/api/v1/search?query=${query}`);
+  return data;
+};
