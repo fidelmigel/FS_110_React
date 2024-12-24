466c8af23b4830081c2906be64e847afbd823ff8
Drynkin Sergey <sergsmile29@gmail.com>
2024-12-24 20:35:18 +0200

    loader and error

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,35 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { fetchArticles } from '../services/api';
+import { FidgetSpinner } from 'react-loader-spinner';
+import Loader from './Loader';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     // axios.get('https://hn.algolia.com/api/v1/search').then(res => setArticles(res.data.hits));
 
     const getArticlesData = async () => {
       try {
+        setIsLoading(true);
+        setIsError(false);
         const { hits } = await fetchArticles('react');
         setArticles(hits);
       } catch (error) {
+        setIsError(true);
         console.log(error);
+      } finally {
+        setIsLoading(false);
       }
     };
     getArticlesData();
   }, []);
 
   return (
-    <div>
+    <div className='main'>
       <h2>Articles</h2>
       <ul>
         {articles.map(item => (
@@ -31,6 +40,8 @@ const App = () => {
           </li>
         ))}
       </ul>
+      {isLoading && <Loader />}
+      {isError && <h2>Something went wrong! Try again...</h2>}
     </div>
   );
 };
