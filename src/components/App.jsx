6cd96a60309ee58794cfd9a7c64c8da37e38a75e
Drynkin Sergey <sergsmile29@gmail.com>
2024-12-24 21:23:58 +0200

    toast and search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,16 @@
-import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { fetchArticles } from '../services/api';
-import { FidgetSpinner } from 'react-loader-spinner';
 import Loader from './Loader';
+import Articles from './Articles/Articles';
+import SearchBar from './SearchBar/SearchBar';
+import toast from 'react-hot-toast';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     // axios.get('https://hn.algolia.com/api/v1/search').then(res => setArticles(res.data.hits));
@@ -16,8 +19,8 @@ const App = () => {
       try {
         setIsLoading(true);
         setIsError(false);
-        const { hits } = await fetchArticles('react');
-        setArticles(hits);
+        const { hits } = await fetchArticles(query, page);
+        setArticles(prev => [...prev, ...hits]);
       } catch (error) {
         setIsError(true);
         console.log(error);
@@ -26,20 +29,30 @@ const App = () => {
       }
     };
     getArticlesData();
-  }, []);
+  }, [page, query]);
 
+  const handleChangePage = () => {
+    toast.success(`Page changed to: ${page + 1}`);
+    setPage(prev => prev + 1);
+  };
+
+  const handleChangeQuery = newQuery => {
+    if (newQuery === query) {
+      toast.error('Please change query!');
+      return;
+    }
+    setQuery(newQuery);
+    setArticles([]);
+    setPage(0);
+  };
   return (
     <div className='main'>
       <h2>Articles</h2>
-      <ul>
-        {articles.map(item => (
-          <li key={item.objectID}>
-            <a href={item.url} target='_blank'>
-              {item.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <SearchBar onSearchChanged={handleChangeQuery} />
+      {articles.length > 0 && <Articles articles={articles} />}
+
+      <button onClick={handleChangePage}>Load more</button>
+
       {isLoading && <Loader />}
       {isError && <h2>Something went wrong! Try again...</h2>}
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,10 @@ import { createRoot } from 'react-dom/client';
 import App from './components/App';
 import './index.css';
 import 'modern-normalize';
-createRoot(document.getElementById('root')).render(<App />);
+import { Toaster } from 'react-hot-toast';
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Toaster />
+  </>
+);
