df91fabde653118026230f0d15b7d1aadde5c636
Drynkin Sergey <sergsmile29@gmail.com>
2024-12-24 03:13:08 +0200

    init m4l1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,4 @@
-import ControlledForm from './Forms/ControlledForm';
-import FormRegister from './Forms/Form';
-
 const App = () => {
-  const onRegister = data => {
-    console.log('Data received!');
-    console.log(data);
-    console.log('User successfully registered!🚀');
-  };
-  return (
-    <div>
-      <h1>FORMIK 🚀</h1>
-      <FormRegister />
-      <ControlledForm onRegister={onRegister} />
-    </div>
-  );
+  return <div></div>;
 };
 export default App;
