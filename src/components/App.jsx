966b77adf84fd9c386ba3483a02af41626ee88cf
Drynkin Sergey <sergsmile29@gmail.com>
2025-01-07 19:52:05 +0200

    Step 1. Install react-router-dom and wrap App by browserRouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import Header from './Header/Header';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route path='/' element={<h1>Hello router</h1>} />
+        <Route path='/about' element={<h2>About page</h2>} />
+      </Routes>
     </div>
   );
 };
