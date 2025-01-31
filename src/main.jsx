169886f93cc2a50e385d4f64754899425fb248c9
Mykhailo Kalinichenko <99479355+fidelmigel@users.noreply.github.com>
2025-01-31 15:29:53 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-import './index.css';
-import 'modern-normalize';
-import { Toaster } from 'react-hot-toast';
-import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import "./index.css";
+import "modern-normalize";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
